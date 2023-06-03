1ec9f445114638091d4c6c49cf6cd2b60dc0a191
muile <tanmui1992@gmail.com>
2023-06-03 14:49:40 +0800

    feat: initial commit

---
+++ src/features/Focus.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native';
+function Focus() {
+  return (
+    <View>
+      <Text> Focus Feature</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Focus;
